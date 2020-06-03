91c402e7f804a3e0d724594a8571e82a35c9f2c9
zachlloyd01 <zachlloyd01@gmail.com>
2020-06-03 15:09:33 -0400

    Comment CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -19,11 +21,9 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
 
     if @post.update(post_params)
       redirect_to @post
@@ -33,11 +33,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
     redirect_to root_path
@@ -47,6 +45,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
